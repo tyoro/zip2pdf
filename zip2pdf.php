c431a19e5b7348aa7266e89c3c6d5d1a89107720
tyoro <orz@tyo.ro>
2012-02-03 01:36:50 +0900

    ガンマ補正が正しく行なわれるようにした。 読み取り時の影でトリムが発生しない時に工夫するようにした。 拡大はされないようにした。

--- zip2pdf.php
+++ zip2pdf.php
@@ -4,9 +4,6 @@ chdir( "/usr/local/sfw/zip2pdf/" );
 
 include_once './conf/load.php';
 
-define( "KINDLE_WIDTH" , 560 );
-define( "KINDLE_HEIGHT", 734 );
-
 $start = time();
 $TEST_MODE = false;
 $output_dir_path = '';
@@ -18,10 +15,10 @@ if( $argc > 1 ){
 			case "--test":
 				$TEST_MODE = true;
 				break;
-			case "--input":
+			case "--input": case "--i":
 				$input_file_name = $argv[++$i];
 				break;
-			case "--output":
+			case "--output": case "--o":
 				$output_dir_path = $argv[++$i];
 				break;
 		}
@@ -64,7 +61,6 @@ if( $argc > 1 ){
 	$canvas = new Imagick();
 	$pdf = new Imagick();
 
-	//for( $i = 0; $i < $za->numFiles && $i < 20 ; $i++ ){
 	for( $i = 0; $i < $za->numFiles ; $i++ ){
 
     	$stat = $za->statIndex( $i );
@@ -81,45 +77,73 @@ if( $argc > 1 ){
 		if( $TEST_MODE && $i%30 ){
 			continue;
 		}
-
 		$image->readImageBlob($za->getFromIndex($i));
 
+		$color = false;
+		// カラー画像の類であるかどうかの判定
 		if( $stat[ "size" ] > $conf['img_threshold'] ){
-		}else{
-			//余白除去
-			$image->trimImage( $conf['trim'] );
+			$color = true;
 		}
 
+		$base_width = $image->getImageWidth(); //横幅（ピクセル）
+		$base_height = $image->getImageHeight(); //縦幅（ピクセル）
+
+		$image->trimImage( 50.0 );
 		$width = $image->getImageWidth(); //横幅（ピクセル）
 		$height = $image->getImageHeight(); //縦幅（ピクセル）
+		if( $base_width == $width && $base_height == $height ){
+			//トリミングされてない(変な枠とかある？
+			print "no trim\n";
 
-		$new_width = KINDLE_WIDTH;
-		$rate = $new_width / $width;
-		$new_height = $rate * $height;
-		if( $new_height > KINDLE_HEIGHT ){
-			$new_height = KINDLE_HEIGHT;
-			$rate = $new_height / $height;
-			$new_width = $rate * $width;
+			$clone = $image->clone();
 
-			$l_margine = (KINDLE_WIDTH - $new_width)/2;
-			$t_margine = 0;
-		}else{
-			$l_margine = 0;
-			$t_margine = (KINDLE_HEIGHT - $new_height)/2;
+			$clone->levelImage(0,1/$conf['gamma'],60535);
+			$clone->cropImage( $base_width-64,$base_height-8	,32,4);
+			$clone->trimImage( 50.0 );
+
+			$width = $clone->getImageWidth(); //横幅（ピクセル）
+			$height = $clone->getImageHeight(); //縦幅（ピクセル）
+			if( $base_width-64 != $width && $base_height-8 != $height ){
+				$image->destroy();
+				$image = $clone;
+			}else{
+				$clone->destroy();
+			}
 		}
 
-		//リサイズ
-		$image->resizeImage( $new_width, $new_height, imagick::FILTER_MITCHELL, 1);
-		//$image->resizeImage( KINDLE_WIDTH, KINDLE_HEIGHT, imagick::FILTER_MITCHELL, 1, true);
-		//$image->setImagePage( KINDLE_WIDTH, KINDLE_HEIGHT, 0, 0 );
+		//拡大される場合
+		if( $conf[ 'width' ] > $width && $conf[ 'height' ] > $height ){
+			$l_margine = ( $conf[ 'width' ] - $width ) /2;
+			$t_margine = ( $conf[ 'height' ] - $height ) /3;
+		}else{
+			$new_width = $conf[ 'width' ];
+			$rate = $new_width / $width;
+			$new_height = $rate * $height;
+			if( $new_height > $conf[ 'height' ] ){
+				$new_height = $conf[ 'height' ];
+				$rate = $new_height / $height;
+				$new_width = $rate * $width;
 
-		$canvas->newImage( KINDLE_WIDTH, KINDLE_HEIGHT, new ImagickPixel("white"));
+				$l_margine = ($conf['width'] - $new_width)/2;
+				$t_margine = 0;
+			}else{
+				$l_margine = 0;
+				$t_margine = ($conf[ 'height' ] - $new_height)/2;
+			}
+			//リサイズ
+			$image->resizeImage( $new_width, $new_height, imagick::FILTER_MITCHELL, 1);
+			}
+		}
+
+		$canvas->newImage( $conf['width'], $conf[ 'height' ], new ImagickPixel("white"));
 		$canvas->compositeImage($image,imagick::COMPOSITE_OVER,$l_margine,$t_margine);
 		$image->clear();
 
 		$canvas->setImageFormat( $ext );
 
-		$im = new Imagick();
+		//level,ガンマ補正
+		//$canvas->gammaImage( 1.0/$conf['gamma'] );
+		$canvas->levelImage(0,1/$conf['gamma'],60535);
 		if( $ext == 'jpg' ){
 			//グレースケール化
 			$canvas->setImageColorspace(Imagick::COLORSPACE_GRAY);
@@ -127,9 +151,8 @@ if( $argc > 1 ){
 			//色深度
 			$canvas->setImageDepth(4);
 		}
-		//ガンマ補正
-		$canvas->setImageGamma( $conf['gamma'] );
 
+		$im = new Imagick();
 		$im->readImageBlob( $canvas->getImageBlob() );
 		$im->setImageFormat( $ext );
 		$pdf->addImage( $im );
